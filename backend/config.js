0dc4151a7eb8f0974f8e08d6814f3f370a502e6f
Shani-001 <sunnysaini6193@gmail.com>
2025-10-04 23:51:16 +0530

    Add updates

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY ="sk_test_51SCM8rJMQOBE2NHqK9NPcJUx2DyVXBvyizfOcfOEzhwf1I22A8BCA1prpaZzBaAkp9nWFKrHa2erDhUBCoNHsv4e00IaB1w0uL"
+const STRIPE_SECRET_KEY =process.env.STRIPE_SECRET_KEY
 export default{
     JWT_USER_PASSWORD,
     JWT_ADMIN_PASSWORD,
