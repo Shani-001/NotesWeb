f004bef526ba0eb1f4702c8d57861fddc2091cf0
Shani-001 <sunnysaini6193@gmail.com>
2025-10-12 16:58:30 +0530

    added mail port

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -3,7 +3,8 @@ import nodemailer from "nodemailer"
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  secure: true, // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass:process.env.EMAIL_PASS,
